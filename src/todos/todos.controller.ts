a7114acee49afcdacfde318f90e5420250aaa81d
jduran <jduran@gen-it.com.ar>
2022-06-26 16:53:39 -0300

    feat(todoController): added endpoints

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Put } from '@nestjs/common';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+   @Get('/')
+   getTodos() {
+      return 'This action returns all todos';
+   }
+
+   @Get('/:id')
+   getTodoById(@Param('id') id: string) {
+      return `This action returns a #${id} todo`;
+   }
+
+   @Put('/:id')
+   updateTodo(@Param('id') id: string) {
+      return `This action updates a #${id} todo`;
+   }
+
+   @Delete('/:id')
+   deleteTodo(@Param('id') id: string) {
+      return `This action removes a #${id} todo`;
+   }
+}
