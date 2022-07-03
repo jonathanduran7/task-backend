a3565280c68f6c05b8046bf26c2259f2ed6ab375
duranjonathan <joniduran624@gmail.com>
2022-07-03 20:59:28 -0300

    feat(user): config entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class UserEntity {}
+import { hash } from "bcryptjs";
+import {
+   BeforeInsert,
+   BeforeUpdate,
+   Column,
+   CreateDateColumn,
+   Entity,
+   PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity('users')
+export class UserEntity {
+   @PrimaryGeneratedColumn()
+   id:number
+
+   @Column({type: 'varchar', length: 255})
+   name: string
+
+   @Column({name: 'last_name', type: 'varchar', length: 255})
+   lastName: string
+
+   @Column({type: 'varchar', length: 255, nullable: false})
+   email: string
+
+   @Column({type: 'varchar', length: 128, nullable: false, select: false})
+   password: string
+
+   @Column({type: 'bool', default: true})
+   status: boolean
+
+   @CreateDateColumn({name: 'created_at', type: 'timestamp'})
+   createdAt: Date
+}
