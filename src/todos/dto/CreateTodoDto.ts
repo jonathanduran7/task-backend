9ebf72035bfd861df051b46ff2f33cec91fac291
jduran <jduran@gen-it.com.ar>
2022-07-03 10:39:45 -0300

    feat(todos): added validation in dto

--- src/todos/dto/CreateTodoDto.ts
+++ src/todos/dto/CreateTodoDto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTodoDto {
+
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
 }
