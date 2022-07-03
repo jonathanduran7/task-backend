86cb3ed1f126fe0c293181bec0e75eac70293ecb
jduran <jduran@gen-it.com.ar>
2022-07-03 18:03:24 -0300

    feat(todoDto): added validation in update

--- src/todos/dto/UpdateTodoDto.ts
+++ src/todos/dto/UpdateTodoDto.ts
@@ -1,5 +1,14 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export class UpdateTodoDto {
+
+   @IsString()
+   @IsNotEmpty()
    title: string;
+
+   @IsString()
    description: string;
+
+   @IsBoolean()
    done: boolean
 }
