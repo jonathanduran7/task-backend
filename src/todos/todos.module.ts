50b390e0c7e65ee60c0347150f02634696080ccc
jduran <jduran@gen-it.com.ar>
2022-07-03 16:47:32 -0300

    feat(todo): implement entity

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './entities/todos.entity';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Todo]),
+  ],
   controllers: [TodosController],
   providers: [TodosService],
 })
