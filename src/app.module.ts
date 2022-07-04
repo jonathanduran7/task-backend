bbdcf02b312c4864b9f3aff9b01ec181c5df8af3
duranjonathan <joniduran624@gmail.com>
2022-07-03 22:40:50 -0300

    feat(appModule): implement config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,19 +8,28 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3310,
-      username: 'admin',
-      password: 'admin',
-      database: 'personal',
-      entities: [__dirname + './**/**/*entity{.ts,.js}'],
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST'),
+        port: parseInt(config.get<string>('DATABASE_PORT'),10),
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        entities: [__dirname + './**/**/*entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logger: 'file',
+        logging: true
+      }),
     }),
     TodosModule,
-    UserModule
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
