996b0985e37289ea19004d70a57403c6c56ec26d
duranjonathan <joniduran624@gmail.com>
2022-07-03 21:38:56 -0300

    feat(user): config dto

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+   @IsOptional()
+   @IsString()
+   @MaxLength(255)
+   name: string;
+
+   @IsOptional()
+   @IsString()
+   @MaxLength(255)
+   lastName: string;
+
+   @IsEmail()
+   email: string;
+
+   @IsString()
+   @MinLength(8)
+   @MaxLength(128)
+   password: string;
+
+}
