d215d83eb69ffb8304ee99d5dc3508c66456e03e
duranjonathan <joniduran624@gmail.com>
2022-07-03 21:08:27 -0300

    feat(userController): created getOne and getMany

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+   constructor(
+      private readonly userService: UserService
+   ){}
+
+   @Get(':id')
+   getOne(@Param('id',ParseIntPipe) id: number){
+      return this.userService.getOne(id)
+   }
+
+   @Get()
+   getMany(){
+      return this.userService.getMany()
+   }
+
+   createOne(){}
+
+}
