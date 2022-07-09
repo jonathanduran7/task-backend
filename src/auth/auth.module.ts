057057da4e4ee3634f3878e163a0a27c6c4a469a
duranjonathan <joniduran624@gmail.com>
2022-07-09 20:39:14 -0300

    feat(app): implement passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
