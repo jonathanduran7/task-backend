e3b17355649d644340843bc05cc3a581c2848bb2
duranjonathan <joniduran624@gmail.com>
2022-07-09 21:12:20 -0300

    feat(auth): validate user in service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+   constructor(
+      private readonly userService: UserService
+   ) { }
+
+   async validateUser(email: string, password: string): Promise<any> {
+      const user = await this.userService.findOne(email)
+
+      if (user && password === user.password) {
+         return user
+      }
+
+      return null
+   }
+}
